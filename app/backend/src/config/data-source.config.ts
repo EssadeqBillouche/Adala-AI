babf3e81938c3dfa9f80466fd6e7a199e23f8d65
essadeq billouche <essadeq.billouche@gmail.com>
2026-03-18 10:36:37 +0000

    Feat: add data source configuration for PostgreSQL database connection

---
+++ app/backend/src/config/data-source.config.ts
@@ -0,0 +1,14 @@
+import { DataSource } from 'typeorm';
+import { config } from 'dotenv';
+config(); // Load the .env file
+
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  url: process.env.Db_URL,
+  synchronize: false,
+  ssl: {
+    rejectUnauthorized: process.env.NODE_ENV === 'production',
+  },
+  entities: ['src/**/*.entity{.ts,.js}'],
+  migrations: ['src/migrations/*{.ts,.js}'],
+});
