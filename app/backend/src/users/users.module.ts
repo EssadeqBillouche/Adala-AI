a258b6043c5c288e7da60508de927215514aaf31
essadeq billouche <essadeq.billouche@gmail.com>
2026-03-30 11:32:42 +0100

    Feat: update UsersModule to include Invitation entities and controllers

--- app/backend/src/users/users.module.ts
+++ app/backend/src/users/users.module.ts
@@ -2,10 +2,15 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersService } from './users.service';
 import { User } from './entities/user.entity';
+import { Invitation } from './entities/invitation.entity';
+import { InvitationsService } from './invitations.service';
+import { InvitationsController } from './invitations.controller';
+import { PublicInvitationsController } from './public-invitations.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
-  providers: [UsersService],
-  exports: [UsersService],
+  imports: [TypeOrmModule.forFeature([User, Invitation])],
+  providers: [UsersService, InvitationsService],
+  controllers: [InvitationsController, PublicInvitationsController],
+  exports: [UsersService, InvitationsService],
 })
 export class UsersModule {}
